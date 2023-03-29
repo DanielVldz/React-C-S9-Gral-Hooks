d85c3c6641b5859a23dc226cae2ec885a57611c7
DanielVz <danielrvp27@gmail.com>
2023-03-28 23:28:16 -0700

    inicio seccion 11 router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { MainApp } from './09-useContext/MainApp'
 import './index.css'
+import { BrowserRouter } from "react-router-dom";
 
 //import { FocusScreen } from './04-useRef/FocusScreen'
 //import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
@@ -15,11 +17,14 @@ import './index.css'
 //import { CallbackHook } from './06-memos/CallbackHook'
 //import { Padre } from './07-tarea-memo/Padre'
 //import './08-useReducer/intro-reducer';
-import { TodoApp } from './08-useReducer/TodoApp';
+//import { TodoApp } from './08-useReducer/TodoApp';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  //<React.StrictMode>
-    <TodoApp />
-  //</React.StrictMode>,
+  <BrowserRouter>
+      {/* <React.StrictMode> */}
+          <MainApp />
+      {/* </React.StrictMode> */}
+  </BrowserRouter>
+
 )
